aa01339b36e4f9b76a0dc99e7ac31eb10b3bc808
Luke Sauls <luke.sauls279@gmail.com>
2025-02-12 16:03:40 -0700

    added navbar and sidebar functions

---
+++ components/Template/NavBar/SideBar/SideBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface SideBarProps {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+const SideBar: React.FC<SideBarProps> = ({ isOpen, toggleSidebar }) => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <div
+        className={`fixed top-0 right-0 h-full bg-[#5A5555] text-[#C8373E] text-lg sm:text-2xl xl:text-3xl font-bold transition-transform duration-300 ${
+          isOpen ? "transform translate-x-0" : "transform translate-x-full"
+        } w-[11rem] md:w-[280px] py-4 px-2 z-40`}
+      ></div>
+      {isOpen && (
+        <div
+          onClick={toggleSidebar}
+          className="fixed inset-0 bg-black opacity-50 z-30"
+        />
+      )}
+    </div>
+  );
+};
+
+export default SideBar;

3afd947b18f08cc9d014b4619adddbe5d1d327d5
Luke Sauls <luke.sauls279@gmail.com>
2025-02-13 15:53:56 -0700

    blog section started, blogs loaded in

--- components/LandingPage/BlogSection/BlogSection.tsx
+++ components/LandingPage/BlogSection/BlogSection.tsx
@@ -1,5 +1,14 @@
-const BlogSection = () => {
-  return <h1>Blog Section</h1>;
+import { getBlogs } from "@/sanity/sanity-utils";
+
+const BlogSection = async () => {
+  const blogs = await getBlogs();
+
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <h3 className="text-red underline underline-offset-[.5rem]">Blog</h3>
+      <div className="flex flex-col gap-4"></div>
+    </div>
+  );
 };
 
 export default BlogSection;
--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -2,18 +2,14 @@ import Header from "./Header";
 import ReviewsSection from "./ReviewsSection";
 import BlogSection from "./BlogSection";
 import MissionStatementSection from "./MissionStatementSection";
-import { getBlogs } from "@/sanity/sanity-utils";
 
 const LandingPage = async () => {
-  const blogs = await getBlogs();
-  console.log(blogs);
-
   return (
     <>
       <Header />
       <MissionStatementSection />
-      <BlogSection />
       <ReviewsSection />
+      <BlogSection />
     </>
   );
 };

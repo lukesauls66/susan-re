15428ab4458dea45f7b0aebd6baa0c749ab8a48b
Matthew Nicholson <thematthewnicholson@gmail.com>
2025-02-13 14:28:35 -0800

    refactor: update image container for responsive design on landing page

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -10,8 +10,10 @@ const LandingPage = async () => {
 
   return (
     <>
-      <div className="relative w-screen">
-        <img src="/images/AZsky.webp" alt="AZ sky" className="w-screen" />
+      <div className="relative w-full">
+        <div className="w-full max-h-[40rem] overflow-hidden">
+          <img src="/images/AZsky.webp" alt="AZ sky" className="w-full" />
+        </div>
         <div className="absolute inset-0 flex flex-col items-center justify-center w-full">
           <div className="flex flex-col items-center justify-between w-[12rem] md:w-[18rem] xl:w-[24rem] h-full py-2 sm:py-4 md:pb-[4rem] md:pt-[8rem] lg:pb-[6rem] lg:pt-[12rem] xl:pb-[8rem] xl:pt-[16rem]">
             <p className="font-bold text-white text-center sm:text-lg md:text-3xl lg:text-4xl xl:text-5xl">

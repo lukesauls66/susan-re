0ec9df58a2ea43ded463e914c9f8e7eee72b4b1a
Luke Sauls <luke.sauls279@gmail.com>
2025-02-13 15:43:46 -0700

    broke header up

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -1,7 +1,7 @@
+import Header from "./Header";
 import ReviewsSection from "./ReviewsSection";
 import BlogSection from "./BlogSection";
 import MissionStatementSection from "./MissionStatementSection";
-import { Button } from "../ui/button";
 import { getBlogs } from "@/sanity/sanity-utils";
 
 const LandingPage = async () => {
@@ -10,26 +10,7 @@ const LandingPage = async () => {
 
   return (
     <>
-      <div className="relative w-full">
-        <div className="w-full max-h-[40rem] overflow-hidden">
-          <img src="/images/AZsky.webp" alt="AZ sky" className="w-full" />
-        </div>
-        <div className="absolute inset-0 flex flex-col items-center justify-center w-full">
-          <div className="flex flex-col items-center justify-between w-[12rem] md:w-[18rem] xl:w-[24rem] h-full py-2 sm:py-4 md:pb-[4rem] md:pt-[8rem] lg:pb-[6rem] lg:pt-[12rem] xl:pb-[8rem] xl:pt-[16rem]">
-            <p className="font-bold text-white text-center sm:text-lg md:text-3xl lg:text-4xl xl:text-5xl">
-              Find your dream home in Arizona
-            </p>
-            <Button className="font-semibold lg:text-lg xl:text-2xl xl:h-[3rem] xl:w-[14rem]">
-              Let&apos;s Explore
-            </Button>
-            <img
-              src="/images/Logo2.svg"
-              alt="Keller Williams Legal Logo"
-              className="bg-white md:hidden"
-            />
-          </div>
-        </div>
-      </div>
+      <Header />
       <MissionStatementSection />
       <BlogSection />
       <ReviewsSection />

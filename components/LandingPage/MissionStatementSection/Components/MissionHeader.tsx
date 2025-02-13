f6b00f3491f833ddf56d411b850040a89826e9b8
Matthew Nicholson <thematthewnicholson@gmail.com>
2025-02-13 10:55:30 -0800

    refactor MissionHeader layout for improved responsiveness and accessibility

--- components/LandingPage/MissionStatementSection/Components/MissionHeader.tsx
+++ components/LandingPage/MissionStatementSection/Components/MissionHeader.tsx
@@ -3,16 +3,12 @@ import React from "react";
 
 const MissionHeader = () => {
   return (
-    <div>
-      <div className="hidden md:block mb-4 w-[85%]">
-        <p className="text-red">About AZ Warrior Real Estate</p>
-        <div className="border-b-2 w-full border-blue" />
-      </div>
+    <div className="flex flex-col md:flex-col-reverse gap-4">
       <h3 className="font-bold text-[40px] leading-none text-blue">
         “Welcome to Your New Home Sweet Home”
       </h3>
       <div className="flex w-full gap-[1rem] justify-center items-center md:items-start md:justify-start md:flex-col-reverse">
-        <div className="md:hidden">
+        <div>
           <p className="text-red">About AZ Warrior Real Estate</p>
           <div className="border-b-2 w-full border-blue" />
         </div>

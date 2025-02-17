fc2ea975540d4592a632035fdbc3f36f59a0e847
Luke Sauls <luke.sauls279@gmail.com>
2025-02-17 10:21:27 -0700

    reviews body init

--- components/ReviewsPage/ReviewsPage.tsx
+++ components/ReviewsPage/ReviewsPage.tsx
@@ -1,9 +1,11 @@
 import ReviewsPageHeader from "./ReviewsPageHeader";
+import ReviewsPageBody from "./ReviewsPageBody";
 
 const ReviewsPage = () => {
   return (
     <div className="p-4">
       <ReviewsPageHeader />
+      <ReviewsPageBody />
     </div>
   );
 };
---
+++ components/ReviewsPage/ReviewsPageBody/ReviewsPageBody.tsx
@@ -0,0 +1,9 @@
+const ReviewsPageBody = () => {
+  return (
+    <div>
+      <h1>Reviews Page Body</h1>
+    </div>
+  );
+};
+
+export default ReviewsPageBody;

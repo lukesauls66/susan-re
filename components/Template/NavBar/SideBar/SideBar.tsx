0a62ce6c231632936f5172a880cde04949daa7cc
Luke Sauls <luke.sauls279@gmail.com>
2025-02-18 16:00:35 -0700

    sidebar links set

--- components/Template/NavBar/SideBar/SideBar.tsx
+++ components/Template/NavBar/SideBar/SideBar.tsx
@@ -10,13 +10,60 @@ interface SideBarProps {
 const SideBar: React.FC<SideBarProps> = ({ isOpen, toggleSidebar }) => {
   const router = useRouter();
 
+  const handleNavigation = (path: string) => {
+    toggleSidebar();
+    router.push(path);
+  };
+
   return (
     <div>
       <div
-        className={`fixed top-0 right-0 h-full bg-grey text-lg sm:text-2xl xl:text-3xl font-bold transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-full bg-grey text-lg md:text-2xl xl:text-3xl font-bold transition-transform duration-300 ${
           isOpen ? "transform translate-x-0" : "transform translate-x-full"
-        } w-[11rem] md:w-[280px] py-4 px-2 z-40`}
-      ></div>
+        } w-[11rem] md:w-[18rem] xl:w-[24rem] py-4 px-2 md:py-[3rem] md:px-4 z-40`}
+      >
+        <div className="flex flex-col items-center justify-between h-full">
+          <div className="flex flex-col items-center gap-10">
+            <a
+              href="#"
+              onClick={() => handleNavigation("/buyer-form")}
+              className="text-white hover:text-red hover:scale-10"
+            >
+              Home Search
+            </a>
+            <a
+              href="#"
+              onClick={() => handleNavigation("/seller-form")}
+              className="text-white hover:text-red hover:scale-10"
+            >
+              Home Valuation
+            </a>
+            <a
+              href="#"
+              onClick={() => handleNavigation("/testimonials")}
+              className="text-white hover:text-red hover:scale-10"
+            >
+              Testimonials
+            </a>
+            <a
+              href="#"
+              onClick={() => handleNavigation("/blog")}
+              className="text-white hover:text-red hover:scale-10"
+            >
+              Blog
+            </a>
+          </div>
+          <div>
+            <a
+              href="#"
+              onClick={() => handleNavigation("/about")}
+              className="text-white hover:text-red hover:scale-10"
+            >
+              About Us
+            </a>
+          </div>
+        </div>
+      </div>
       {isOpen && (
         <div
           onClick={toggleSidebar}

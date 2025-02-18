ab43360f005d258dc6c6260dfc014650658406cd
Luke Sauls <luke.sauls279@gmail.com>
2025-02-18 16:37:09 -0700

    capitalized page component

--- app/buyer-form/page.tsx
+++ app/buyer-form/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import SellerFromBody from "./components/SellerFormBody";
 import FormHeader from "../../components/FormComponents/FormHeader";
 
-const page = () => {
+const Page = () => {
   return (
     <div className="max-w-[50rem] mx-auto flex flex-col gap-8 p-6 mb-[2rem] bg-blue rounded-md text-white shadow-md">
       <FormHeader pageTitle="Let's Find Your Dream Home" />
@@ -11,4 +11,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;

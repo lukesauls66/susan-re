0185df73b90b331d2fee04e98bfdbde821a92a7b
glowupmatt <glowupmatt@gmail.com>
2025-02-20 08:48:19 -0800

    add mobile design to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,15 +12,15 @@ async function AboutPage() {
       <h1 className="text-center font-semibold text-[2rem] mb-[2rem]">
         About Me
       </h1>
-      <div className="flex align-center justify-center gap-4">
-        <div className="h-96 w-96 rounded-md overflow-hidden aspect-square">
+      <div className="flex flex-col md:flex-row align-center justify-center gap-4">
+        <div className="md:h-96 md:w-96 rounded-md overflow-hidden aspect-square">
           <img
             src={urlFor(aboutData.imageUrl).url()}
             alt={aboutData.name}
             className="h-full w-full object-cover"
           />
         </div>
-        <div className="w-[40%]">
+        <div className="md:w-[40%]">
           <h3 className="font-bold text-[3rem]">{aboutData.name}</h3>
           <p>{aboutData.description}</p>
         </div>

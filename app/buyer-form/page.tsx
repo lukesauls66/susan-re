c207d6b845189408019709c1b6f8b5497140be69
Matthew Nicholson <thematthewnicholson@gmail.com>
2025-02-20 20:49:38 -0800

    add padding around form pages

--- app/buyer-form/page.tsx
+++ app/buyer-form/page.tsx
@@ -4,9 +4,11 @@ import FormHeader from "../../components/FormComponents/FormHeader";
 
 const Page = () => {
   return (
-    <div className="max-w-[50rem] mx-auto flex flex-col gap-8 p-6 mb-[2rem] bg-blue rounded-md text-white shadow-md">
-      <FormHeader pageTitle="Let's Find Your Dream Home" />
-      <BuyerFormBody />
+    <div className="p-4">
+      <div className="max-w-[50rem] mx-auto flex flex-col gap-8 p-6 mb-[2rem] bg-blue rounded-md text-white shadow-md">
+        <FormHeader pageTitle="Let's Find Your Dream Home" />
+        <BuyerFormBody />
+      </div>
     </div>
   );
 };

9c4b0637bb2a2b029513d6d5098fc06ffdb9d4fe
Luke Sauls <luke.sauls279@gmail.com>
2025-03-05 20:18:54 -0700

    clean up code

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,6 @@
 import LandingPage from "@/components/LandingPage";
 import { getLandingPageData, LandingPageData } from "@/sanity/sanity-utils";
 
-// interface HomeProps {
-//   landingPageData: LandingPageData;
-// }
-
-// export async function getServerSideProps() {
-//   const landingPageData = await getLandingPageData();
-
-//   return {
-//     props: {
-//       landingPageData,
-//     },
-//   };
-// }
-
 export default async function Home() {
   const landingPageData: LandingPageData = await getLandingPageData();
 

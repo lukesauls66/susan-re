fae3a93e29d32ff0f6b2c0ac7ec86f0cca9d2d3f
Luke Sauls <luke.sauls279@gmail.com>
2025-03-05 18:40:52 -0700

    sanity schema for landing page done

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 import { blogType } from "./blogType";
 import { reviewType } from "./reviewType";
 import { aboutMeType } from "./aboutMeType";
+import { landingPageType } from "./landingPageType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blogType, reviewType, aboutMeType],
+  types: [blogType, reviewType, aboutMeType, landingPageType],
 };

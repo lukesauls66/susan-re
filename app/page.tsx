412a197de14a82c85d247e9879ea461b0a498bbf
Luke Sauls <luke.sauls279@gmail.com>
2025-03-05 20:06:58 -0700

    ensured SSR for landing page fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import LandingPage from "@/components/LandingPage";
+import { getLandingPageData, LandingPageData } from "@/sanity/sanity-utils";
 
-export default function Home() {
-  return <LandingPage />;
+interface HomeProps {
+  landingPageData: LandingPageData;
+}
+
+export async function getServerSideProps() {
+  const landingPageData = await getLandingPageData();
+
+  return {
+    props: {
+      landingPageData,
+    },
+  };
+}
+
+export default function Home({ landingPageData }: HomeProps) {
+  return <LandingPage landingPageData={landingPageData} />;
 }
--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -2,21 +2,10 @@ import Header from "./Header";
 import ReviewsSection from "./ReviewsSection";
 import BlogSection from "./BlogSection";
 import MissionStatementSection from "./MissionStatementSection";
-import { getLandingPageData } from "@/sanity/sanity-utils";
 import { LandingPageData } from "@/sanity/sanity-utils";
 
 interface LandingPageProps {
   landingPageData: LandingPageData;
-}
-
-export async function getServerSideProps() {
-  const landingPageData = await getLandingPageData();
-
-  return {
-    props: {
-      landingPageData,
-    },
-  };
 }
 
 const LandingPage: React.FC<LandingPageProps> = ({ landingPageData }) => {

7c4b2a4acb97277529054d214a6db9590fcfdd4b
Luke Sauls <luke.sauls279@gmail.com>
2025-03-05 20:10:45 -0700

    ensured SSR for landing page fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 import LandingPage from "@/components/LandingPage";
 import { getLandingPageData, LandingPageData } from "@/sanity/sanity-utils";
 
-interface HomeProps {
-  landingPageData: LandingPageData;
-}
+// interface HomeProps {
+//   landingPageData: LandingPageData;
+// }
 
-export async function getServerSideProps() {
-  const landingPageData = await getLandingPageData();
+// export async function getServerSideProps() {
+//   const landingPageData = await getLandingPageData();
 
-  return {
-    props: {
-      landingPageData,
-    },
-  };
-}
+//   return {
+//     props: {
+//       landingPageData,
+//     },
+//   };
+// }
 
-export default function Home({ landingPageData }: HomeProps) {
+export default async function Home() {
+  const landingPageData: LandingPageData = await getLandingPageData();
+
   return <LandingPage landingPageData={landingPageData} />;
 }

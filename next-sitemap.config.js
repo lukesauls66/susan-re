dd673aacec17cd4b476d5152f0bb9c7585884f54
William Lyman <luke.sauls279@gmail.com>
2025-04-22 16:58:27 -0700

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.azwarriorre.com",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  exclude: ["/studio/*"],
+};
